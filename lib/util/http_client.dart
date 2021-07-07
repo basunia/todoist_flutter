5e72be5c3454bb8f9981d90e446e3ee328e42134
Mahmud Basunia <mahmud.basunia@gmail.com>
2021-07-07 23:29:04 +0600

    initial commit

--- lib/util/http_client.dart
+++ lib/util/http_client.dart
@@ -1,15 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class Http {
-  static Dio dio;
+  static Dio? dio;
 
-  static Dio getDio() {
+  static Dio? getDio() {
     if (dio == null) {
       dio = new Dio();
-      dio.options.baseUrl = "https://staging-data.gtaf.org/api/";
-      dio.options.connectTimeout = 5000; //5s
-      dio.options.receiveTimeout = 3000;
-      dio.interceptors.add(PrettyDioLogger(
+      dio?.options.baseUrl = "https://staging-data.gtaf.org/api/";
+      dio?.options.connectTimeout = 5000; //5s
+      dio?.options.receiveTimeout = 3000;
+      dio?.interceptors.add(PrettyDioLogger(
           requestHeader: true,
           requestBody: true,
           responseBody: true,
@@ -17,8 +18,8 @@ class Http {
           error: true,
           compact: true,
           maxWidth: 90));
-      dio.options.headers["Api-Key"] = "7z*)_-iulu_6j20!h@r8lw8g4)";
-      dio.options.headers["Accept-Language"] = 'bn';
+      dio?.options.headers["Api-Key"] = "7z*)_-iulu_6j20!h@r8lw8g4)";
+      dio?.options.headers["Accept-Language"] = 'bn';
     }
 
     return dio;
