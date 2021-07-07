23601c6e9fbd792b03b5545853bba55c8efe0bb4
Mahmud Basunia <mahmud.basunia@gmail.com>
2021-07-07 09:50:52 +0600

    added http client

---
+++ lib/util/http_client.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+
+class Http {
+  static Dio dio;
+
+  static Dio getDio() {
+    if (dio == null) {
+      dio = new Dio();
+      dio.options.baseUrl = "https://staging-data.gtaf.org/api/";
+      dio.options.connectTimeout = 5000; //5s
+      dio.options.receiveTimeout = 3000;
+      dio.interceptors.add(PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseBody: true,
+          responseHeader: false,
+          error: true,
+          compact: true,
+          maxWidth: 90));
+      dio.options.headers["Api-Key"] = "7z*)_-iulu_6j20!h@r8lw8g4)";
+      dio.options.headers["Accept-Language"] = 'bn';
+    }
+
+    return dio;
+  }
+}
